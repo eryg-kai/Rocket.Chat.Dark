3a5e9ccdf91c0f5c277922d1e7943007c3de6409
dev@0x0049.me <dev@0x0049.me>
2017-02-05 12:14:38 -0700

    Add a custom build process
    This is for custom styling that you don't want in Git, like
    styling specific user names.

--- README.md
+++ README.md
@@ -6,7 +6,18 @@ A dark Discord-inspired theme for Rocket.Chat
 2. Go to Administration > Layout in Rocket.Chat
 3. Paste the copied contents into "Custom CSS"
 
+### Custom additions
+Custom styling that can't or shouldn't be in source control goes in
+`src/custom.styl`. If you use this, you'll need to run the build or development
+steps below and copy `dist/custom.css` to Rocket.Chat instead of
+`dist/dark.css`. `src/custom.styl` should have `@import "dark"` as the first
+line.
+
+## Building
+1. `npm install`
+2. `npm run build`
+
 ## Development
 1. `npm install`
-2. `npm run gulp watch` (or `npm run gulp` to build once)
+2. `npm run gulp watch`
 3. Edit source in `src`
--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var autoprefixer = require('gulp-autoprefixer');
 var clean = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var stylus = require('gulp-stylus');
+var fs = require('fs');
 
+// This generates the standard works-for-everyone CSS files that get committed.
 gulp.task('dark', function () {
 	return gulp.src('src/dark.styl')
 		.pipe(stylus())
@@ -21,8 +23,28 @@ gulp.task('dark', function () {
 		.pipe(gulp.dest('dist'));
 });
 
+// This generates a custom build that shouldn't be in source control. Only runs
+// if src/custom.styl exists.
+gulp.task('custom', function () {
+	if (fs.existsSync('src/custom.styl')) {
+		return gulp.src('src/custom.styl')
+			.pipe(stylus())
+			.pipe(autoprefixer({
+				browsers: ['last 2 versions', 'ie 8', 'ie 9'],
+				cascade: false,
+				remove: false
+			}))
+			.pipe(gulp.dest('dist'))
+			.pipe(rename('custom.min.css'))
+			.pipe(clean({
+				compatibility: 'ie8'
+			}))
+			.pipe(gulp.dest('dist'));
+	}
+});
+
 gulp.task('watch', function () {
 	gulp.watch('src/dark*', ['dark']);
 });
 
-gulp.task('default', ['dark']);
+gulp.task('default', ['dark', 'custom']);
